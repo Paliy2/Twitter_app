9694d24dc2662ab59938c26ece2afc0020a99bae
Yewgen_Dom <52251850+Paliy2@users.noreply.github.com>
2020-02-21 20:35:11 +0200

    Add files via upload

---
+++ web_map.py
@@ -0,0 +1,49 @@
+import folium
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+
+
+def generate_map(name_loc):
+    '''
+    dict -> ()
+    {name: loc}
+    '''
+
+    map = folium.Map()
+    name_layer = folium.FeatureGroup(name="name's layer")
+    for key in name_loc.keys():
+        name_layer.add_child(folium.Marker(location=name_loc[key],
+                                           tooltip=key))
+    map.add_child(name_layer)
+    map.add_child(folium.LayerControl())
+    map.save('templates/Map.html')
+    print('Check map in tests/Map.html')
+
+
+def get_coordinates(data):
+    '''
+    dict -> list
+    Transfer every location name into coordinates
+    '''
+
+    # create geolocator
+    geolocator = Nominatim(user_agent="Romanyk", timeout=3)
+    geocode = RateLimiter(geolocator.geocode, min_delay_seconds=0.1)
+
+    all_locs = {}
+    for key in data.keys():
+        location = geocode(data[key])
+
+        if location:
+            all_locs[key] = (location.latitude, location.longitude)
+    return all_locs
+
+
+def create_map(data):
+    # data = json_to_dict(file)
+    data = get_coordinates(data)
+    generate_map(data)
+
+
+
+
